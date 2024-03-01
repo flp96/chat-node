3d41c96e172190800706e08bfad443a55c92ef23
flp96 <filipefernandes__@live.com>
2024-03-01 18:17:21 -0300

    implementando nodejs primeira parte

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('expresse');
 const path = require('path');
 const http = require('http');
-
+const socketIO = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const PORT = 4000;
 app.use(express.static(path.join(__dirname, 'public')));
 server.listen(PORT, () => "Servidor online na porta " + PORT);
+
+const io = socketIO(server);  
